49c6955bfc16c63ff00f86231e42046e053f5c61
Anders Strömberg <lynxz@Lynxz.(none)>
2013-07-28 20:05:04 +0200

    Adding initial code base.

---
+++ creepytweety/TweetClient.py
@@ -0,0 +1,168 @@
+'''
+Created on Jan 4, 2013
+
+@author: lynxz
+'''
+
+import oauth2 as oauth
+import httplib2
+import urlparse
+import json
+from time import time, sleep
+
+USER_TIMELINE_URL = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+
+CONSUMER_KEY = 'HAgdrAVwYrtn4enya9ow'
+CONSUMER_SECRET = 'kyhCttZf1mL8VJ2P1hEvZPIr35WbsX98mgGb8xTg4'
+
+TOKEN_FILE = 'access_tokens.dat'
+
+class TweetClient(oauth.Client):
+    '''
+    This class can be used to fetch twitter tweets from a specific individual
+    '''
+
+    def __init__(self, sign_method = oauth.SignatureMethod_HMAC_SHA1(), cache=None, timeout=None, proxy_info=None):
+        '''
+        Constructor
+        '''
+        self.consumer = oauth.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
+        super(TweetClient, self).__init__(oauth.Consumer(CONSUMER_KEY, CONSUMER_SECRET))
+        
+        self.ACCESS_TOKEN_KEY = ''
+        self.ACCESS_TOKEN_SECRET = ''
+        self.sign_method = sign_method
+        self.limit_reached = False
+        self.renewed_limit_time = 0.0
+        
+    
+    def __read_tokens(self):
+        with open(TOKEN_FILE, 'r') as f:
+            self.ACCESS_TOKEN_KEY = f.readline().rstrip('\n')
+            self.ACCESS_TOKEN_SECRET = f.readline().rstrip('\n') 
+    
+    def __write_tokens(self):
+        with open(TOKEN_FILE, 'w') as f:
+            f.write(self.ACCESS_TOKEN_KEY + '\n')
+            f.write(self.ACCESS_TOKEN_SECRET + '\n')
+
+    def __get_tokens(self):
+
+        request_token_url = 'http://twitter.com/oauth/request_token'
+        access_token_url = 'http://twitter.com/oauth/access_token'
+        authorize_url = 'http://twitter.com/oauth/authorize'
+
+        resp, content = self.request(request_token_url, "GET")
+        if resp['status'] != '200':
+            raise Exception("Invalid response %s." % resp['status'])
+
+        request_token = dict(urlparse.parse_qsl(content))
+
+        print "Request Token:"
+        print "    - oauth_token        = %s" % request_token['oauth_token']
+        print "    - oauth_token_secret = %s" % request_token['oauth_token_secret']
+        print 
+
+        print "Go to the following link in your browser:"
+        print "%s?oauth_token=%s" % (authorize_url, request_token['oauth_token'])
+        print 
+
+        accepted = 'n'
+        while accepted.lower() == 'n':
+            accepted = raw_input('Have you authorized me? (y/n) ')
+        oauth_verifier = raw_input('What is the PIN? ')
+
+        self.token = oauth.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
+        self.token.set_verifier(oauth_verifier)
+
+        resp, content = self.request(access_token_url, "POST")
+        access_token = dict(urlparse.parse_qsl(content))
+
+        print "Access Token:"
+        print "    - oauth_token        = %s" % access_token['oauth_token']
+        print "    - oauth_token_secret = %s" % access_token['oauth_token_secret']
+        print
+        print "You may now access protected resources using the access tokens above." 
+        print
+        
+        self.ACCESS_TOKEN_KEY = access_token['oauth_token']
+        self.ACCESS_TOKEN_SECRET = access_token['oauth_token_secret']
+        self.__write_tokens()
+        
+    
+    def __fetch_data(self, url, http_method = "GET", body = '', headers = None):
+        '''
+        Fetches data from a specific url using the specified http_method. Body and headers can also be supplied.
+        Raises an exception if the http call fails.
+        '''
+        
+        if self.limit_reached:
+            sleep_time_in_seconds = self.renewed_limit_time - time()
+            if sleep_time_in_seconds > 0:
+                print 'sleeping for %.2f.' % (sleep_time_in_seconds / 60.0)
+                sleep(int(sleep_time_in_seconds) + 60)
+        
+        resp, content = httplib2.Http.request(self, url, method=http_method, body=body, headers=headers)
+        self.renewed_limit_time = float(resp['x-rate-limit-reset'])
+        print 'Remaining queries: %s' % resp['x-rate-limit-remaining']
+        print 'Remaining time for limit reset: %.2f' % ((float(resp['x-rate-limit-reset']) - time()) / 60.0)
+        
+        if int(resp['x-rate-limit-remaining']) == 0:
+            sleep_time_in_seconds = self.renewed_limit_time - time()
+            if sleep_time_in_seconds > 0:
+                print 'sleeping for %.2f.' % (sleep_time_in_seconds / 60.0)
+                sleep(int(sleep_time_in_seconds) + 60)
+        
+        if resp['status'] != '200':
+            raise IOError("Invalid Response %s." % resp['status'])
+        return content
+        
+    
+    def __create_signed_request(self, url, http_method = "GET", body = '', parameters = {}, headers = None):
+        req = oauth.Request.from_consumer_and_token(self.consumer,
+                                                    token=self.token, 
+                                                    http_method=http_method, 
+                                                    http_url=url,
+                                                    parameters=parameters, 
+                                                    body=body)
+        req.sign_request(self.sign_method, self.consumer, self.token)
+        return req
+
+    def setup(self):
+        try:
+            self.__read_tokens() #Reads tokens from a file
+        except:
+            self.__get_tokens() #Does the Twitter/OAuth three legged authentication
+            
+        self.token = oauth.Token(self.ACCESS_TOKEN_KEY, self.ACCESS_TOKEN_SECRET)
+            
+
+    def fetch_user_tweets(self, user_name, count = 20):
+        parameters = {'screen_name': user_name,}
+        data = []
+        max_id = -1
+
+        if count > 3200:
+            raise Exception('Twitter 1.1 REST API only allows 3200 GET calls to a user timeline. %d is to many.' % count)
+            
+        while count > 0:
+            if count > 200:
+                parameters['count'] = 200
+                count -= 200
+            else:
+                parameters['count'] = count
+                count = 0
+            
+            if max_id != -1:
+                max_id -= 1  #By removing one we avoid getting the same tweet twice.
+                parameters['max_id'] = max_id
+                
+            req = self.__create_signed_request(USER_TIMELINE_URL, parameters = parameters)
+            partial_data = json.loads(self.__fetch_data(req.to_url()))
+            data.extend(partial_data)
+            if len(partial_data) == 200:
+                max_id = long(partial_data[199]['id'])
+            
+        return data
+        
+    
---
+++ creepytweety/generateGraph.py
@@ -0,0 +1,155 @@
+'''
+Created on Jan 4, 2013
+
+@author: lynxz
+'''
+
+from TweetClient import TweetClient
+import networkx as nx
+import matplotlib.pyplot as plt
+import operator
+
+def get_tweets(user, tweets):
+    client = TweetClient()
+    client.setup()
+    try:
+        return client.fetch_user_tweets(user, tweets)
+    except IOError as e:
+        print 'Caught IOError %s' % e.strerror
+        return []
+
+def get_user_info(tweet):
+    user_data = {'name': tweet['user']['name'],
+                 'screen_name': tweet['user']['screen_name'],
+                 'id': tweet['user']['id'],
+                 'profile_image': tweet['user']['profile_image_url'],
+                 'location': tweet['user']['location']}
+    
+    if tweet['user']['url'] != None:
+        if tweet['user']['entities']['url']['urls'][0]['expanded_url'] != None:
+            user_data['url'] = tweet['user']['entities']['url']['urls'][0]['expanded_url']
+        else:
+            user_data['url'] = tweet['user']['url']
+        
+    return user_data
+
+def get_tweet_data(tweet):
+    tweet_data = {'message' : tweet['text'],
+                  'id': tweet['id'],
+                  'user_mentions': tweet['entities']['user_mentions'],
+                  'hashtags': tweet['entities']['hashtags']
+                }
+
+    return tweet_data
+
+def print_statistics(stat_dict):
+    print '** Statistics **'
+    print ''
+        
+    norm_value = 1.0
+        
+    if len(stat_dict) > 0:
+        print "%d entries used" % len(stat_dict)
+        sorted_entries = sorted(stat_dict.iteritems(), key = operator.itemgetter(1), reverse = True)
+        for entry in sorted_entries:
+            print '%s : %d' % entry
+        norm_value = float(sorted_entries[0][1] * 2.0)
+        print ''
+    return norm_value
+
+def generate_hashtag_graph(user, tweet_count):
+    tweets = get_tweets(user, tweet_count)
+
+    if len(tweets) > 0:
+        user_data = get_user_info(tweets[0])
+        sn = user_data['screen_name']
+        hashtag_usage = {}
+
+        print "User id: %s Screen Name: %s Real Name: %s" % (user_data['id'], user_data['screen_name'], user_data['name'])
+        
+        for tweet in tweets:
+            tweet_data = get_tweet_data(tweet)
+                    
+            for hashtag in tweet_data['hashtags']:
+                if hashtag['text'] in hashtag_usage: 
+                    hashtag_usage[hashtag['text']] += 1
+                else:
+                    hashtag_usage[hashtag['text']] = 1
+        
+        norm_value = print_statistics(hashtag_usage)
+    
+        print "** Generating Graph **"
+        
+        hashtag_graph = nx.Graph()
+        hashtag_graph.add_node(sn)
+        
+        for hashtag in hashtag_usage.keys():
+            hashtag_graph.add_node(hashtag)
+            hashtag_graph.add_edge(sn, hashtag, weight = float(hashtag_usage[hashtag]) / norm_value)
+            
+        print ''
+            
+        pos=nx.spring_layout(hashtag_graph)
+        plt.figure(figsize = (20, 20))
+        plt.title(user_data['name'])
+        nx.draw(hashtag_graph, pos, edge_color = 'b', alpha = 0.5, node_size = 0, iterations = 200)
+        plt.axis('equal')
+        plt.savefig('%s_hashtag.png' % sn)
+        plt.show()
+
+def add_user_interactions_to_graph(user, tweet_count, graph, treshold):
+    tweets = get_tweets(user, tweet_count)
+    user_interactions = {}
+    return_list = []
+    
+    if len(tweets) > 0:
+        user_data = get_user_info(tweets[0])
+        sn = user_data['screen_name']
+        
+        print "User id: %s Screen Name: %s Real Name: %s" % (user_data['id'], sn, user_data['name'])
+        
+        for tweet in tweets:
+            tweet_data = get_tweet_data(tweet)
+                    
+            for user in tweet_data['user_mentions']:
+                if user['screen_name'] in user_interactions: 
+                    user_interactions[user['screen_name']] += 1
+                else:
+                    user_interactions[user['screen_name']] = 1
+    
+        norm_value = print_statistics(user_interactions)
+        
+        for user in user_interactions.keys():
+            if user_interactions[user] >= treshold:
+                graph.add_node(user)
+                graph.add_edge(sn, user, weight = (float(user_interactions[user]) / norm_value))
+                return_list.append(user)
+    
+    return return_list
+            
+def plot_interaction_graph(graph, user):
+    pos=nx.spring_layout(graph)
+    plt.figure(figsize = (16.5, 11.7))
+    plt.title('Social interactions for %s' % user)
+    nx.draw(graph, pos, edge_color = 'b', alpha = 0.5, node_size = 0, iterations = 100)
+    plt.axis('equal')
+    plt.savefig('%s_interactions.png' % user)
+    print 'Done!'
+
+def generate_interactions_graph_for_users(users, tweet_count, current_level, max_level, graph, treshold, processed_users = []):
+    for user in users:
+        if not user in processed_users:
+            processed_users.append(user)
+            user_list = add_user_interactions_to_graph(user, tweet_count, graph, treshold)
+            if current_level < max_level:
+                generate_interactions_graph_for_users(user_list, tweet_count, current_level + 1, max_level, graph, treshold, processed_users)  
+
+def generate_interactions_graph(user, tweet_count = 100, max_level = 2, treshold = 1):
+    interaction_graph = nx.DiGraph()
+    interaction_graph.add_node(user)
+    generate_interactions_graph_for_users([user,], tweet_count, 0, max_level, interaction_graph, treshold)
+    plot_interaction_graph(interaction_graph, user)
+
+if __name__ == '__main__':
+    generate_interactions_graph('MedborgareX', tweet_count = 100, max_level = 1, treshold = 2)
+        
